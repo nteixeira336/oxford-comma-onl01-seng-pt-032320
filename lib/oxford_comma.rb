9a36aa4c850a254e24ab17bae9d4ed5f015122c6
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-04-01 12:35:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,9 @@ require 'pry'
 def oxford_comma(array)
   if array.length == 1 
     array.join 
-    elsif array.length ==2 
-  array.join (" and ")
-else array.push[-1] = "and #{array[-1]}"
-  array.join (", ")
+  elsif array.length ==2 
+    array.join (" and ")
+  else array.push[-1] = "and #{array[-1]}"
+    array.join (", ")
+  end 
 end
